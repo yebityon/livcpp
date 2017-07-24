9684d4fd1af0941f304da2de3df0d28629a49f7a
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-24 15:52:18 +0000

    Update

--- algorithm/binarySearch.cpp
+++ algorithm/binarySearch.cpp
@@ -1,9 +1,9 @@
-// Find the maximum x∈[low, high) such that f(x) is true
+// Find the maximum x∈[low, up) such that f(x) is true
 template<typename F>
-int binarySearch(int low, int high, const F &f) {
-  while (high - low > 1) {
-    int mid                  = low + (high - low) / 2;
-    *(f(mid) ? &low : &high) = mid;
+int binarySearch(int low, int up, const F &f) {
+  while (up - low > 1) {
+    int mid             = low + (up - low) / 2;
+    (f(mid) ? low : up) = mid;
   }
   return low;
 }
