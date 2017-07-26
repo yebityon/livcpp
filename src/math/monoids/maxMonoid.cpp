cdf2f9ad937fa29b9d155b52168a24bf8f29ba88
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-26 11:03:03 +0000

    Update

--- src/math/monoids/maxMonoid.cpp
+++ src/math/monoids/maxMonoid.cpp
@@ -1,2 +1,2 @@
-template<int lowerInf>
+template<int lowerInf = std::numeric_limits<int>::min() / 8>
 Monoid<int> maxMonoid = {lowerInf, [](int a, int b) { return max(a, b); }};
--- src/math/monoids/minMonoid.cpp
+++ src/math/monoids/minMonoid.cpp
@@ -1,2 +1,2 @@
-template<int upperInf>
+template<int upperInf = std::numeric_limits<int>::max() / 8>
 Monoid<int> minMonoid = {upperInf, [](int a, int b) { return min(a, b); }};
