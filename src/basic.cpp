7665d3ef3f4039846ce32030b9de5d2678939104
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-27 15:31:10 +0000

    Update

--- src/basic.cpp
+++ src/basic.cpp
@@ -3,21 +3,20 @@
 #define main signed main()
 #define loop(i, a, n) for (int i = (a); i < (n); i++)
 #define rep(i, n) loop(i, 0, n)
-#define forever while (true)
+#define forever for (;;)
 #define all(v) (v).begin(), (v).end()
 #define rall(v) (v).rbegin(), (v).rend()
 #define prec(n) fixed << setprecision(n)
-using pii = std::pair<int, int>;
-using vi = std::vector<int>;
-using vd = std::vector<double>;
-using vc = std::vector<char>;
-using vb = std::vector<bool>;
-using vs = std::vector<std::string>;
-using vpii = std::vector<pii>;
-using vvi = std::vector<vi>;
-using vvb = std::vector<vb>;
-using vvpii = std::vector<vpii>;
-template<typename A> using fn = std::function<A>;
+template<typename A> using V = std::vector<A>;
+template<typename A> using F = std::function<A>;
+template<typename A, typename B> using P = std::pair<A, B>;
+using pii = P<int, int>;
+using vi = V<int>;
+using vd = V<double>;
+using vs = V<std::string>;
+using vpii = V<pii>;
+using vvi = V<vi>;
+using vvpii = V<vpii>;
 constexpr int INF = sizeof(int) == sizeof(long long) ? 1000000000000000000LL : 1000000000;
 constexpr int MOD = 1000000007;
 constexpr double PI = acos(-1);
--- src/cpp11/basic.cpp
+++ src/cpp11/basic.cpp
@@ -3,22 +3,21 @@
 #define main signed main()
 #define loop(i, a, n) for (int i = (a); i < (n); i++)
 #define rep(i, n) loop(i, 0, n)
-#define forever while (true)
+#define forever for (;;)
 #define all(v) (v).begin(), (v).end()
 #define rall(v) (v).rbegin(), (v).rend()
 #define prec(n) fixed << setprecision(n)
 typedef std::pair<int, int> pii;
 typedef std::vector<int> vi;
 typedef std::vector<double> vd;
-typedef std::vector<char> vc;
-typedef std::vector<bool> vb;
 typedef std::vector<std::string> vs;
 typedef std::vector<pii> vpii;
 typedef std::vector<vi> vvi;
-typedef std::vector<vb> vvb;
 typedef std::vector<vpii> vvpii;
 const int INF = sizeof(int) == sizeof(long long) ? 1000000000000000000LL : 1000000000;
 const int MOD = 1000000007;
 const double PI = acos(-1);
 template<typename A, typename B> bool cmin(A &a, const B &b) { return a > b ? (a = b, true) : false; }
 template<typename A, typename B> bool cmax(A &a, const B &b) { return a < b ? (a = b, true) : false; }
+
+main { cout << 57 << endl; }
