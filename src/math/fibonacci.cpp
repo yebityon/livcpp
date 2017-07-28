ccc5f5f19d3de1dc2b54edfef8612f1d44c7cffd
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-28 08:19:24 +0000

    Add fibonacci

---
+++ src/math/fibonacci.cpp
@@ -0,0 +1,9 @@
+template<typename Integral = int> Integral fibonacci(const int &n) {
+  static std::vector<Integral> dp(2, 1);
+  int prev = dp.size();
+  if (prev < n + 1) {
+    dp.resize(n + 1);
+    loop(i, prev, dp.size()) dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+}
