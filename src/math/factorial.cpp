d367b3fcbf2c2bd2b1bf27f565969ffcdfa80d26
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-28 08:19:11 +0000

    Update factorial

--- src/math/factorial.cpp
+++ src/math/factorial.cpp
@@ -1,7 +1,7 @@
 template<typename Integral = int> Integral factorial(const int &n) {
   static std::vector<Integral> dp(1, 1);
   int prev = dp.size();
-  if (prev < n) {
+  if (prev < n + 1) {
     dp.resize(n + 1);
     loop(i, prev, dp.size()) dp[i] = dp[i - 1] * i;
   }
