5b3ee967b0e259bb7cbe0d683634d4610458c5fa
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-30 16:45:57 +0000

    Set default typename

--- src/monoids/gcdMonoid.cpp
+++ src/monoids/gcdMonoid.cpp
@@ -1,4 +1,4 @@
-template<typename T> struct gcdMonoid {
+template<typename T = int> struct gcdMonoid {
   using value_type = T;
   constexpr T id() const { return 0; }
   T operator()(const T &a, const T &b) const { return gcd(a, b); }
--- src/monoids/lcmMonoid.cpp
+++ src/monoids/lcmMonoid.cpp
@@ -1,4 +1,4 @@
-template<typename T> struct lcmMonoid {
+template<typename T = int> struct lcmMonoid {
   using value_type = T;
   constexpr T id() const { return 1; }
   T operator()(const T &a, const T &b) const { return lcm(a, b); }
