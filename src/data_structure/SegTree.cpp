80c8982e234903296c668a4d7cf8353e00aae28f
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-30 10:56:59 +0000

    Update

--- src/data_structure/SegTree.cpp
+++ src/data_structure/SegTree.cpp
@@ -2,40 +2,40 @@ template<typename Monoid> class SegTree {
   using T = typename Monoid::value_type;
 
   Monoid m;
-  std::vector<T> data;
+  std::vector<T> tree;
   int size = 1;
 
-  void _update(int i) { data[i] = m(data[i * 2], data[i * 2 + 1]); }
+  void _update(int i) { tree[i] = m(tree[i * 2], tree[i * 2 + 1]); }
 
 public:
   SegTree(const int &n = 0) {
     m = Monoid();
     while (size < n) size *= 2;
-    data.assign(size * 2, m.id());
+    tree.assign(size * 2, m.id());
   }
 
   template<typename InputIterator> SegTree(InputIterator first, InputIterator last) {
     m = Monoid();
     int n = std::distance(first, last);
     while (size < n) size *= 2;
-    data.resize(size * 2, m.id());
-    std::copy(first, last, data.begin() + size);
+    tree.resize(size * 2, m.id());
+    std::copy(first, last, tree.begin() + size);
     for (int i = size - 1; i >= 1; i--) _update(i);
   }
 
   T fold(int l, int r) const { // [l, r)
     T acc = m.id();
     for (l += size, r += size; l < r; l /= 2, r /= 2) {
-      if (l & 1) acc = m(acc, data[l++]);
-      if (r & 1) acc = m(acc, data[--r]);
+      if (l & 1) acc = m(acc, tree[l++]);
+      if (r & 1) acc = m(acc, tree[--r]);
     }
     return acc;
   }
 
   void update(int i, const T &x) {
-    data[i += size] = x;
+    tree[i += size] = x;
     while (i /= 2) _update(i);
   }
 
-  const T &operator[](int i) const { return data[i + size]; }
+  const T &operator[](int i) const { return tree[i + size]; }
 };
