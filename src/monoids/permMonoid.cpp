f6bc4e410d7528ceac1091d7d4fdec8b82ceda06
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-30 17:13:13 +0000

    Add permMonoid

---
+++ src/monoids/permMonoid.cpp
@@ -0,0 +1,13 @@
+template<int size, typename T> struct permMonoid {
+  using value_type = T;
+  constexpr T id() const {
+    T v(size);
+    iota(v.begin(), v.end(), 0);
+    return v;
+  }
+  T operator()(const T &a, const T &b) const {
+    T v(size);
+    rep(i, size) v[i] = b[a[i]];
+    return v;
+  }
+};
