8c0641e1a48fa3cea4a4de40042ba8bfcca48b7b
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-07-30 17:13:00 +0000

    Fix bug

--- src/monoids/powm.cpp
+++ src/monoids/powm.cpp
@@ -1,6 +1,6 @@
-template<typename Monoid> constexpr int powm(typename Monoid::value_type a, int b) {
+template<typename Monoid> constexpr typename Monoid::value_type powm(typename Monoid::value_type a, int b) {
   Monoid m = Monoid();
-  int c = m.id();
+  typename Monoid::value_type c = m.id();
   while (b) {
     if (b & 1) c = m(c, a);
     b >>= 1;
