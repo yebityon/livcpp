6b83a10f835c73d36f214bee619aeef864887941
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-08-02 07:57:38 +0000

    a

--- src/monoid/fold.cpp
+++ src/monoid/fold.cpp
@@ -1,4 +1,4 @@
-template<class Monoid, class InputIterator> typename Monoid::value_type fold(InputIterator first, InputIterator last) {
+template<typename Monoid, typename InputIterator> typename Monoid::value_type fold(InputIterator first, InputIterator last) {
   Monoid m = Monoid();
   auto acc = m.id();
   std::for_each(first, last, [&](auto &x) { acc = m(acc, x); });
--- src/monoid/foldMap.cpp
+++ src/monoid/foldMap.cpp
@@ -1,4 +1,4 @@
-template<class Monoid, class InputIterator, class F> typename Monoid::value_type foldMap(InputIterator first, InputIterator last, const F &f) {
+template<typename Monoid, typename InputIterator, typename F> typename Monoid::value_type foldMap(InputIterator first, InputIterator last, const F &f) {
   Monoid m = Monoid();
   auto acc = m.id();
   std::for_each(first, last, [&](auto &x) { acc = m(acc, f(x)); });
