a551046a2de9c0ebf130620a2a59f2711a7304b0
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-08-03 17:32:50 +0000

    Add countInversions

---
+++ src/general/countInversions.cpp
@@ -0,0 +1,9 @@
+template<typename InputIterator> int countInversions(InputIterator first, InputIterator last) {
+  FenwickTree<sumGroup<int>> ft(distance(first, last));
+  int c = 0;
+  for (int i = 0; first != last; i++, first++) {
+    c += i - ft.fold(*first);
+    ft.add(*first, 1);
+  }
+  return c;
+}
