3c851bf8ca8dc60e8cbfa75696db195c9127095f
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-08-03 00:39:33 +0000

    Update

--- src/graph/bellmanFord.cpp
+++ src/graph/bellmanFord.cpp
@@ -1,4 +1,4 @@
-template<int inf = std::numeric_limits<Weight>::max() / 8> std::pair<std::vector<Weight>, bool> bellmanFord(const Graph &g, int src) {
+template<int inf = std::numeric_limits<Weight>::max() / 8> std::pair<std::vector<Weight>, std::vector<int>> bellmanFord(const Graph &g, int src) {
   int n = g.size();
   Edges es;
   for (auto &adj : g) {
@@ -6,18 +6,14 @@ template<int inf = std::numeric_limits<Weight>::max() / 8> std::pair<std::vector
   }
   std::vector<Weight> dist(n, inf);
   dist[src] = 0;
-  bool negCycle = false;
-  for (int i = 0;; i++) {
-    bool updated = false;
+  std::vector<int> negCycle(n);
+  rep(i, n) {
     for (auto &e : es) {
       if (dist[e.src] != inf) {
-        if (cmin(dist[e.dst], dist[e.src] + e.weight)) updated = true;
+        if (cmin(dist[e.dst], dist[e.src] + e.weight)) {
+          if (i == n - 1) negCycle[e.dst] = true;
+        }
       }
-    }
-    if (!updated) break;
-    if (i > n) {
-      negCycle = true;
-      break;
     }
   }
   return {dist, negCycle};
