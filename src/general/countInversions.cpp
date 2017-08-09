0001bbbae5ee4afd0726d86a82474acba5f163c8
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-08-09 08:26:55 +0000

    Fix bug

--- src/general/countInversions.cpp
+++ src/general/countInversions.cpp
@@ -1,5 +1,5 @@
 template<typename InputIterator> int countInversions(InputIterator first, InputIterator last) {
-  FenwickTree<sumGroup<int>> ft(distance(first, last));
+  FenwickTree<sumGroup<int>> ft(*max_element(first, last));
   int c = 0;
   for (int i = 0; first != last; i++, first++) {
     c += i - ft.fold(*first);
