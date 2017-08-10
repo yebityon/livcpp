681519d69df5945f0e5338c797eecd3f442545c5
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-08-10 18:11:24 +0000

    Update

--- src/basic.cpp
+++ src/basic.cpp
@@ -23,6 +23,8 @@ constexpr int MOD = 1000000007;
 constexpr double PI = acos(-1);
 template<typename A, typename B> bool cmin(A &a, const B &b) { return a > b ? (a = b, true) : false; }
 template<typename A, typename B> bool cmax(A &a, const B &b) { return a < b ? (a = b, true) : false; }
+constexpr bool odd(const int &n) { return n & 1; }
+constexpr bool even(const int &n) { return ~n & 1; }
 template<typename T> std::istream &operator>>(std::istream &is, std::vector<T> &v) { for (T &x : v) is >> x; return is; }
 template<typename A, typename B> std::istream &operator>>(std::istream &is, std::pair<A, B> &p) { is >> p.first; is >> p.second; return is; }
 using namespace std;
