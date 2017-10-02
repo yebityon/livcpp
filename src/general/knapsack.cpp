844042070c0fc8b8b6169ad5f79ce78d997e22af
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-10-02 10:10:35 +0000

    Add knapsack

---
+++ src/general/knapsack.cpp
@@ -0,0 +1,13 @@
+int knapsack(int items, int maxW, const std::vector<int> &ws, const std::vector<int> &vs) {
+  std::map<pii, int> dp;
+  F<int(int, int)> f = [&](int i, int j) {
+    pii p = {i, j};
+    if (dp.count(p)) return dp[p];
+    if (i == items) return dp[p] = 0;
+    int r = f(i + 1, j);
+    if (j - ws[i] >= 0) cmax(r, f(i + 1, j - ws[i]) + vs[i]);
+    dp[p] = r;
+    return r;
+  };
+  return f(0, maxW);
+}
