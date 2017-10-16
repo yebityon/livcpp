ca271bb6b2cd99c8af4e8000e195bba17cbe0793
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-10-16 17:42:38 +0000

    Update

--- src/flow/fordFulkerson.cpp
+++ src/flow/fordFulkerson.cpp
@@ -1,5 +1,5 @@
 template<int inf = std::numeric_limits<Flow>::max() / 8> Flow fordFulkerson(FlowGraph g, const int source, const int sink) {
-  std::vector<int> used;
+  std::vector<int> used(g.size());
   std::function<int(int, int, int)> dfs = [&](int s, int t, int f) -> int {
     if (s == t) return f;
     used[s] = true;
@@ -16,7 +16,7 @@ template<int inf = std::numeric_limits<Flow>::max() / 8> Flow fordFulkerson(Flow
   };
   Flow s = 0;
   for (;;) {
-    used.assign(g.size(), false);
+    std::fill(used.begin(), used.end(), false);
     Flow f = dfs(source, sink, inf);
     if (f == 0) return s;
     s += f;
