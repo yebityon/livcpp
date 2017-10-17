680000dc4e7e4bbb39a4565432261e8bfe23eb5b
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-10-17 12:32:09 +0000

    Update

--- src/flow/fordFulkerson.cpp
+++ src/flow/fordFulkerson.cpp
@@ -1,10 +1,10 @@
 // Change it to 'FlowGraph &g' when you need a residual network
 template<Flow inf = std::numeric_limits<Flow>::max() / 8> Flow fordFulkerson(FlowGraph g, const int source, const int sink) {
   std::vector<int> used(g.size());
-  std::function<Flow(int, Flow)> dfs = [&](int s, Flow f) -> Flow {
-    if (s == sink) return f;
-    used[s] = true;
-    for (auto &e : g[s]) {
+  std::function<Flow(int, Flow)> dfs = [&](int v, Flow f) -> Flow {
+    if (v == sink) return f;
+    used[v] = true;
+    for (auto &e : g[v]) {
       if (used[e.dst] || e.cap <= 0) continue;
       Flow d = dfs(e.dst, min(f, e.cap));
       if (d > 0) {
