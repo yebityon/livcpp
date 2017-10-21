137fb72dea850e9282091b260a0f8c87b54c7fcb
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2017-10-21 09:05:57 +0000

    Add linearMonoid

---
+++ src/math/monoid/instances/linearMonoid.cpp
@@ -0,0 +1,5 @@
+template<typename T> struct linearMonoid {
+  using value_type = std::pair<T, T>;
+  constexpr value_type id() { return {1, 0}; }
+  constexpr value_type operator()(const value_type &a, const value_type &b) { return {b.first * a.first, b.first * a.second + b.second}; }
+};
