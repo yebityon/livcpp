79db85294e55346603e18d943c4d022ca9b608ea
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-03-01 11:38:09 +0000

    Add RangeFold

---
+++ src/data-structures/RangeFold.cpp
@@ -0,0 +1,20 @@
+template<typename CommutativeGroup> class RangeFold {
+  using T = typename CommutativeGroup::value_type;
+
+  CommutativeGroup g;
+
+  std::vector<T> cache;
+
+public:
+  RangeFold(const std::vector<T> &raw) : cache(raw) {
+    size_t w = cache.size();
+    loop(i, 1, w) cache[i] += cache[i - 1];
+  }
+
+  T fold(const int l, const int r) { // [l, r)
+    T acc = g.id();
+    if (r > 0) acc = g(acc, cache[r - 1]);
+    if (l > 0) acc = g(acc, g.inv(cache[l - 1]));
+    return acc;
+  }
+};
