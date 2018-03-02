8739b1872927b276e618addbe52e28a3ed04d53a
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-03-02 02:28:46 +0000

    Fix typo

--- src/math/monoid/instances/anyMonoid.cpp
+++ src/math/monoid/instances/anyMonoid.cpp
@@ -1,4 +1,4 @@
-template<typename T = bool> struct andMonoid {
+template<typename T = bool> struct anyMonoid {
   using value_type = T;
   constexpr value_type id() { return false; }
   constexpr value_type operator()(const value_type &a, const value_type &b) { return a | b; }
