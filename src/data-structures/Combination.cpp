6c54095b9534069bd6680ff42422487cad1b3f2d
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-03-06 15:15:25 +0000

    Update Combination

--- src/data-structures/Combination.cpp
+++ src/data-structures/Combination.cpp
@@ -1,13 +1,21 @@
 class Combination {
+  int r;
   std::vector<int> data;
 
 public:
-  Combination(int n, int r) {
+  Combination(int n, int r) : r(r) {
     data.resize(n);
     std::fill(data.end() - (n - r), data.end(), 1);
   }
 
   bool next() { return std::next_permutation(data.begin(), data.end()); }
 
-  int operator[](int i) { return !data[i]; }
+  std::vector<int> get() {
+    std::vector<int> v;
+    v.reserve(r);
+    rep(i, data.size()) if ((*this)[i]) v.emplace_back(i);
+    return v;
+  }
+
+  bool operator[](int i) { return !data[i]; }
 };
