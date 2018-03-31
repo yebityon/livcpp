203178091ffa4b2142e349a99fef413b6dc462d5
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-03-31 14:46:49 +0000

    Add ternarySearch

---
+++ src/general/ternarySearch.cpp
@@ -0,0 +1,12 @@
+template<int precision = 400, typename F> double ternarySearch(const double &l, const double &r, const F &f) { return ternarySearch<precision>(l, r, f, std::less<double>()); }
+template<int precision = 400, typename F, typename C> double ternarySearch(double l, double r, const F &f, const C &c) {
+  rep(i, precision) {
+    double m1 = (l * 2.0 + r) / 3.0, m2 = (l + r * 2.0) / 3.0;
+    if (c(f(m1), f(m2))) {
+      r = m2;
+    } else {
+      l = m1;
+    }
+  }
+  return (l + r) / 2.0;
+}
