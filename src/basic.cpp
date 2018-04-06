ff256a06a34a54256d41602296d4987e2f1ea60f
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-04-06 23:48:57 +0000

    Update

--- src/basic.cpp
+++ src/basic.cpp
@@ -1,23 +1,13 @@
 // clang-format off
 #include <bits/stdc++.h>
+using namespace std;
 #define int long long
 #define main signed main()
 #define loop(i, a, n) for (int i = (a); i < (n); i++)
 #define rep(i, n) loop(i, 0, n)
-#define forever for (;;)
 #define all(v) (v).begin(), (v).end()
 #define rall(v) (v).rbegin(), (v).rend()
 #define prec(n) fixed << setprecision(n)
-template<typename A> using V = std::vector<A>;
-template<typename A> using F = std::function<A>;
-template<typename A, typename B> using P = std::pair<A, B>;
-using pii = P<int, int>;
-using vi = V<int>;
-using vd = V<double>;
-using vs = V<std::string>;
-using vpii = V<pii>;
-using vvi = V<vi>;
-using vvpii = V<vpii>;
 constexpr int INF = sizeof(int) == sizeof(long long) ? 1000000000000000000LL : 1000000000;
 constexpr int MOD = 1000000007;
 constexpr double PI = 3.14159265358979;
@@ -25,12 +15,15 @@ template<typename A, typename B> bool cmin(A &a, const B &b) { return a > b ? (a
 template<typename A, typename B> bool cmax(A &a, const B &b) { return a < b ? (a = b, true) : false; }
 constexpr bool odd(const int n) { return n & 1; }
 constexpr bool even(const int n) { return ~n & 1; }
-template<typename T> std::istream &operator>>(std::istream &is, std::vector<T> &v) { for (T &x : v) is >> x; return is; }
-template<typename A, typename B> std::istream &operator>>(std::istream &is, std::pair<A, B> &p) { is >> p.first >> p.second; return is; }
-template<typename Head, typename Value> auto vectors(const Head &head, const Value &v) { return std::vector<Value>(head, v); }
-template<typename Head, typename... Tail> auto vectors(Head x, Tail... tail) { auto inner = vectors(tail...); return std::vector<decltype(inner)>(x, inner); }
-template<typename T = int, typename... Args> T in(Args &&... args) { T x(args...); std::cin >> x; return x; }
-using namespace std;
+template<typename T = int> T in() { T x; cin >> x; return x; }
+template<typename T = int> T in(T &&x) { T z(forward<T>(x)); cin >> z; return z; }
+template<typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &x : v) is >> x; return is; }
+template<typename A, typename B> istream &operator>>(istream &is, pair<A, B> &p) { return is >> p.first >> p.second; }
+template<typename T> ostream &operator<<(ostream &os, const vector<vector<T>> &v) { int n = v.size(); rep(i, n) os << v[i] << (i == n - 1 ? "" : "\n"); return os; }
+template<typename T> ostream &operator<<(ostream &os, const vector<T> &v) { int n = v.size(); rep(i, n) os << v[i] << (i == n - 1 ? "" : " "); return os; }
+template<typename A, typename B> ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << p.first << ' ' << p.second; }
+template<typename Head, typename Value> auto vectors(const Head &head, const Value &v) { return vector<Value>(head, v); }
+template<typename Head, typename... Tail> auto vectors(Head x, Tail... tail) { auto inner = vectors(tail...); return vector<decltype(inner)>(x, inner); }
 // clang-format on
 
 main {
