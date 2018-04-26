eeecfdf6cd2e0c51b73c0886f6b9fcaa2fb1994e
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-04-26 16:08:58 +0000

    Update

--- src/data-structures/UnionFind.cpp
+++ src/data-structures/UnionFind.cpp
@@ -2,11 +2,11 @@ class UnionFind {
   vector<int> tree, rank, _size;
 
 public:
-  UnionFind(const int n) : tree(n), rank(n), _size(n, 1) { iota(all(tree), 0); }
+  UnionFind(const int &n) : tree(n), rank(n), _size(n, 1) { iota(all(tree), 0); }
 
-  int root(const int x) { return tree[x] == x ? x : tree[x] = root(tree[x]); }
+  int root(const int &x) { return tree[x] == x ? x : tree[x] = root(tree[x]); }
 
-  bool same(const int a, const int b) { return root(a) == root(b); }
+  bool same(const int &a, const int &b) { return root(a) == root(b); }
 
   bool unite(int a, int b) {
     a = root(a), b = root(b);
@@ -18,7 +18,7 @@ public:
     return true;
   }
 
-  int sizeOf(int x) { return _size[root(x)]; }
+  int sizeOf(const int &x) { return _size[root(x)]; }
 
   int size() { return tree.size(); }
 };
