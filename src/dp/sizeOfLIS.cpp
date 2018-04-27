52013901325a3b58db1819ea72ca12e071d71e73
vvataarne <is0399ii@ed.ritsumei.ac.jp>
2018-04-27 05:08:40 +0000

    Update

--- src/dp/sizeOfLIS.cpp
+++ src/dp/sizeOfLIS.cpp
@@ -1,7 +1,7 @@
-template<typename T> vector<T> sizeOfLIS(const vector<T> &v) {
+template<typename T> int sizeOfLIS(const vector<T> &v) {
   int n = v.size();
   const int inf = numeric_limits<T>::max();
   vector<T> dp(n, inf);
   rep(i, n) * lower_bound(dp.begin(), dp.end(), v[i]) = v[i];
-  return vector<T>(dp.begin(), lower_bound(dp.begin(), dp.end(), inf));
+  return distance(dp.begin(), lower_bound(dp.begin(), dp.end(), inf));
 }
